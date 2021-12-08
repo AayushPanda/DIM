e97a110c2dda5ad105a1414370740b21e6d85298
Aayush Panda <aayush.vinayak@gmail.com>
2021-12-07 20:59:01 -0500

    completed text replacement logic, added code for user ids

--- background.js
+++ background.js
@@ -1,14 +1,29 @@
-function getUrl(){
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-        console.log(tabs[0].url);
-    });
+function getRandomToken() {
+    // E.g. 8 * 32 = 256 bits token
+    var randomPool = new Uint8Array(32);
+    crypto.getRandomValues(randomPool);
+    var hex = '';
+    for (var i = 0; i < randomPool.length; ++i) {
+        hex += randomPool[i].toString(16);
+    }
+    // E.g. db18458e2782b2b77e36769c569e263a53885a9944dd0a861e5064eac16f1a
+    return hex;
 }
 
-chrome.tabs.onUpdated.addListener(((tabId, change, tab) => {
-    if (tab.active && change.url && !"channel" in change.url) {
-        console.log(change.url);
-    }
-    chrome.tabs.executeScript({
-        file: 'contentScript.js'
+chrome.runtime.onInstalled.addListener(function() {
+    userid = getRandomToken();
+    chrome.storage.sync.set({userid: userid}, function() {
+        useToken(userid);
     });
+});
+
+chrome.tabs.onUpdated.addListener(((tabId, change, tab) => {
+    if (change.url.includes('youtube')) {
+        chrome.tabs.executeScript({
+            file: 'contentScript.js'
+        });
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, change.url, function(response) {});
+        });
+    }
 }));
--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,35 @@
 matchHTML = '<yt-formatted-string id="text" class="style-scope ytd-toggle-button-renderer style-text">Dislike</yt-formatted-string>';
 
 var textboxes = document.getElementsByClassName('style-scope ytd-toggle-button-renderer style-text');
-for (var i = 0, l = textboxes.length; i < l; i++) {
-    if(textboxes[i].outerHTML === matchHTML){
-        textboxes[i].innerText = 'DEADBEEF';
+
+var dislikes = 0;
+
+function getRandomToken() {
+    // E.g. 8 * 32 = 256 bits token
+    var randomPool = new Uint8Array(32);
+    crypto.getRandomValues(randomPool);
+    var hex = '';
+    for (var i = 0; i < randomPool.length; ++i) {
+        hex += randomPool[i].toString(16);
     }
+    // E.g. db18458e2782b2b77e36769c569e263a53885a9944dd0a861e5064eac16f1a
+    return hex;
 }
+
+chrome.runtime.onMessage.addListener(
+    function(message, sender, sendResponse) {
+        for (var i = 0, l = textboxes.length; i < l; i++) {
+            if(textboxes[i].outerHTML === matchHTML || textboxes[i].id === 'dislikes'){
+                //get dislike count from firebase
+                textboxes[i].innerHTML = dislikes;
+                textboxes[i].id = 'dislikes'
+            }
+        }
+        sendResponse({});
+    }
+);
+
+//On dislike click
+chrome.storage.local.get("userid", (result) => {
+    // Push/pull from db with dislikes
+});
